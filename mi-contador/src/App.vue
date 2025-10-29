e3cf1a1e825c1625f21d103cb0788509ca29f855
Carlos Daniel Aguilar Chirinos <caguilarc@ulasalle.edu.pe>
2025-10-29 13:59:52 -0500

    Agregando proyecto

---
+++ mi-contador/src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import Counter from './components/Counter.vue'
+</script>
+
+<template>
+  <div id="app">
+    <Counter />
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
